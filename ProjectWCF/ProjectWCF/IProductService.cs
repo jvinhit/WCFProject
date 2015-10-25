11ce02366c7e9758fbe451778e88d2f5cdbc37c8
jvinhit <jvinhit@gmail.com>
2015-10-25 10:48:16 +0700

    Signed-off-by: jvinhit <jvinhit@gmail.com>

--- ProjectWCF/ProjectWCF/IProductService.cs
+++ ProjectWCF/ProjectWCF/IProductService.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
 using System.Text;
+using System.Xml;
 
 namespace ProjectWCF
 {
@@ -12,16 +16,75 @@ namespace ProjectWCF
     public interface IProductService
     {
         [OperationContract]
+        [ReferencePreservingDataContractFormat]
         List<Produto> FindAll();
         [OperationContract]
+        [ReferencePreservingDataContractFormat]
         Produto Find(int id);
         [OperationContract]
-        Produto New (Produto pro);
+        Produto New(Produto pro);
         [OperationContract]
         Produto Update(Produto pro);
         [OperationContract]
         Produto DeleteId(int id);
         [OperationContract]
         Produto Delete(Produto pro);
+    }
+    public class ReferencePreservingDataContractFormatAttribute
+       : Attribute, IOperationBehavior
+    {
+        #region IOperationBehavior Members
+        public void AddBindingParameters(OperationDescription description,
+            BindingParameterCollection parameters)
+        {
+        }
+
+        public void ApplyClientBehavior(OperationDescription description,
+            ClientOperation proxy)
+        {
+            IOperationBehavior innerBehavior =
+              new ReferencePreservingDataContractSerializerOperationBehavior(description);
+            innerBehavior.ApplyClientBehavior(description, proxy);
+        }
+
+        public void ApplyDispatchBehavior(OperationDescription description,
+            DispatchOperation dispatch)
+        {
+            IOperationBehavior innerBehavior =
+              new ReferencePreservingDataContractSerializerOperationBehavior(description);
+            innerBehavior.ApplyDispatchBehavior(description, dispatch);
+        }
+
+        public void Validate(OperationDescription description)
+        {
+        }
+
+        #endregion
+    }
+
+
+
+    public class ReferencePreservingDataContractSerializerOperationBehavior :
+        DataContractSerializerOperationBehavior
+    {
+        #region Ctor
+        public ReferencePreservingDataContractSerializerOperationBehavior(
+            OperationDescription operationDescription)
+            : base(operationDescription) { }
+        #endregion
+
+        #region Public Methods
+
+        public override XmlObjectSerializer CreateSerializer(Type type,
+               XmlDictionaryString name, XmlDictionaryString ns,
+               IList<Type> knownTypes)
+        {
+            return new DataContractSerializer(type, name, ns, knownTypes,
+                2147483646 /*maxItemsInObjectGraph*/,
+                false/*ignoreExtensionDataObject*/,
+                true/*preserveObjectReferences*/,
+                null/*dataContractSurrogate*/);
+        }
+        #endregion
     }
 }
