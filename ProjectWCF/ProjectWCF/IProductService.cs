eaf3ca4f5d99275d25f1820519766a24438d12f3
jvinhit <jvinhit@gmail.com>
2015-10-25 10:40:01 +0700

    Signed-off-by: jvinhit <jvinhit@gmail.com>

---
+++ ProjectWCF/ProjectWCF/IProductService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProjectWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IProductService" in both code and config file together.
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        List<Produto> FindAll();
+        [OperationContract]
+        Produto Find(int id);
+        [OperationContract]
+        Produto New (Produto pro);
+        [OperationContract]
+        Produto Update(Produto pro);
+        [OperationContract]
+        Produto DeleteId(int id);
+        [OperationContract]
+        Produto Delete(Produto pro);
+    }
+}
---
+++ ProjectWCF/ProjectWCF/ProductService.svc.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProjectWCF
+{
+    // jvinhit@gmail.com
+    public class ProductService : IProductService
+    {
+        ProjetoModeloDBEntities _db;
+        public List<Produto> FindAll()
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                List<Produto> productos = _db.Produto.Include("Cliente").ToList();
+                return productos;
+            }
+        }
+
+        public Produto Find(int id)
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                Produto pro = _db.Produto.Single(x => x.ProdutoId.Equals(id));
+                return pro;
+            }
+        }
+
+        public Produto New(Produto pro)
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                _db.Produto.Add(pro);
+                _db.SaveChanges();
+                return pro;
+            }
+        }
+
+        public Produto Update(Produto pro)
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                _db.Entry(pro).State = EntityState.Modified;
+                _db.SaveChanges();
+                return pro;
+            }
+        }
+
+        public Produto DeleteId(int id)
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                Produto pro = _db.Set<Produto>().Find(id);
+                _db.Set<Produto>().Remove(pro);
+                _db.SaveChanges();
+                return pro;
+            }
+        }
+
+        public Produto Delete(Produto pro)
+        {
+            using (_db = new ProjetoModeloDBEntities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                _db.Set<Produto>().Remove(pro);
+                _db.SaveChanges();
+                return pro;
+            }
+        }
+    }
+}
